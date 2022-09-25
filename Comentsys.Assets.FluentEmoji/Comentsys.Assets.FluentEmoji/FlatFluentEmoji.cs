114aa79ba29b9b18339a41b768c02fcebb4cdb34
Peter Bull <peter.bull@ninesoftware.co.uk>
2022-09-25 16:22:35 +0100

    Initial Release

---
+++ Comentsys.Assets.FluentEmoji/Comentsys.Assets.FluentEmoji/FlatFluentEmoji.cs
@@ -0,0 +1,48 @@
+﻿namespace Comentsys.Assets.FluentEmoji;
+
+/// <summary>
+/// Emoji designed by Microsoft - a collection of familiar, friendly, and modern emoji. License: MIT License
+/// </summary>
+public class FlatFluentEmoji : AssetBase<FlatFluentEmoji>
+{
+    private const int width = 32;
+    private const int height = 32;
+    private const string folder = "Flat";
+    private const string root = "Comentsys.Assets.FluentEmoji.Resources";
+
+    /// <summary>
+    /// Path
+    /// </summary>
+    /// <param name="type">Fluent Emoji Type</param>
+    /// <returns>Fluent Emoji Path</returns>
+    private static string Path(FluentEmojiType type) =>
+        $"{folder}.{Enum.GetName(typeof(FluentEmojiType), type) ?? string.Empty}";
+
+    /// <summary>
+    /// Get Asset Resource
+    /// </summary>
+    /// <param name="type">Fluent Emoji Type</param>
+    /// <returns>Asset Resource</returns>
+    public static AssetResource Get(FluentEmojiType type) =>
+        new(AsStream(root, Path(type)) ?? new MemoryStream(), height, width);
+
+    /// <summary>
+    /// Get Asset Resource
+    /// </summary>
+    /// <param name="type">Fluent Emoji Type</param>
+    /// <param name="source">Source Colour</param>
+    /// <param name="target">Target Colour</param>
+    /// <returns>Asset Resource</returns>
+    public static AssetResource Get(FluentEmojiType type, Color? source, Color? target) =>
+        new(AsStream(root, Path(type), source, target) ?? new MemoryStream(), height, width);
+
+    /// <summary>
+    /// Get Asset Resource
+    /// </summary>
+    /// <param name="type">Fluent Emoji Type</param>
+    /// <param name="sources">Source Colours</param>
+    /// <param name="targets">Target Colours</param>
+    /// <returns>Asset Resource</returns>
+    public static AssetResource Get(FluentEmojiType type, Color[]? sources, Color[]? targets) =>
+        new(AsStream(root, Path(type), sources, targets) ?? new MemoryStream(), height, width);
+}
---
+++ NUGET.md
@@ -0,0 +1,22 @@
+# Comentsys.Assets.FluentEmoji
+
+**Comentsys.Assets.FluentEmoji** is an **Asset Resource** in **.NET Standard** for the **Flat** style of **Fluent Emoji** created by **Microsoft** available from [github.com/microsoft/fluentui-emoji](https://github.com/microsoft/fluentui-emoji) and uses **Comentsys.Toolkit**.
+
+## Documentation and Source Code
+
+**Documentation** and **Source Code** for **Comentsys.Assets.FluentEmoji** can be found at [github.com/Comentsys/Comentsys.Assets.FluentEmoji](https://github.com/Comentsys/Comentsys.Assets.FluentEmoji)
+
+## NuGet
+
+You can add **Comentsys.Assets.FluentEmoji** to your project from [nuget.org](https://nuget.org) by using the following command in the **Package Manager**:
+
+```
+Install-Package Comentsys.Assets.FluentEmoji
+```
+
+## Change Log
+
+### Version 1.0.0
+
+- Initial Release
+
